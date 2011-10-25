2419d664ba70f9a78d71589acd7f930372087fc9
Marcel Schneider <marcel.schneider@studi.informatik.uni-stuttgart.de>
2011-10-25 09:25:20 +0200

    Hallo Ada!

---
+++ hallo_ada.adb
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------
+-- hallo_ada.adb
+-- PSE Übung 1
+-- Version:	1
+-- Datum:	25. 10. 2011
+-- Autoren: 	Unbekannt/viele
+-----------------------------------------------------------------------
+
+with Ada.Text_IO;
+use Ada.Text_IO;
+procedure hallo_ada is
+begin
+   Put_Line("Hallo Ada!" );
+end hallo_ada;
+
+-- kate: indent-width 3; indent-mode normal;
