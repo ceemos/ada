c74f40d7f5be6c95296fd3bbd9e10de6ad1a1501
Marcel Schneider <m.schneider.neuffen@gmail.com>
2011-10-25 20:47:08 +0200

    Fakultät

---
+++ fakultaet.adb
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------
+-- fakultaet.adb
+-- PSE Übung 1
+-- Version:	1
+-- Datum:	25. 10. 2011
+-- Autoren: 	Marcel Schneider
+-----------------------------------------------------------------------
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
+procedure Fakultaet is
+   N,
+   Fak: Natural;
+   
+   grenzeUnten, grenzeOben: Natural;
+begin
+   Put_Line("Geben Sie die untere Grenze an:");
+   Get(grenzeUnten);
+   Put_Line("Geben Sie die obere Grenze an:");
+   Get(grenzeOben);
+   
+   Put_Line("   N | Fak");
+   for k in grenzeUnten..grenzeOben loop
+      N := k;
+      Fak := 1;
+      for i in 1.. N loop
+         Fak := Fak*i;
+      end loop;
+      
+      -- Augabe einer Zeile
+      Put(k, 4); -- 4 Stellen n
+      Put(" | ");
+      Put(Fak, 10); -- 10 Stellen Ergebnis
+      New_Line;
+      
+   end loop;
+end Fakultaet;
+
+
+-- kate: indent-width 3; indent-mode normal; dynamic-word-wrap on; line-numbers on; space-indent on; mixed-indent off
