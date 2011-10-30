b39d7720a633c67228513ff05677947cd7798ebd
Marcel Schneider <m.schneider.neuffen@gmail.com>
2011-10-30 23:14:46 +0100

    Weihnachtsberechnung -- geht soweit

---
+++ weihnachten.adb
@@ -0,0 +1,81 @@
+-----------------------------------------------------------------------
+-- weihnachten.adb
+-- PSE 
+-- Version:	1
+-- Datum:	30. 10. 2011
+-- Autoren: 	Marcel Schneider
+-----------------------------------------------------------------------
+
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
+procedure weihnachten is
+   
+   -- prueft, ob das uebergebene Datum Heilig Abend ist
+   function ist_weihnachten(t, m: Natural) return boolean is
+   begin
+      return t = 24 and m = 12;
+   end ist_weihnachten;
+   
+   -- prueft, ob das uebergebene Jahr ein Schaltjahr ist.
+   function ist_schaltjahr(j: Natural) return boolean is
+   begin
+      return ((j mod 4) = 0 and (j mod 100) /= 0) 
+           or (j mod 400) = 0;
+   end ist_schaltjahr;
+   
+   procedure addiere_einen_Tag(t, m, j: in out Natural) is
+   begin
+      
+      -- Sonderfall Februar
+      if m = 2 then
+         if    (t = 28 and not ist_schaltjahr(j))
+            or (t = 29 and     ist_schaltjahr(j)) then
+            t := 0;
+            m := 3;
+         end if;
+      end if;     
+      -- Monatswechsel bei 30 Tegen
+      if t = 30 and (m = 4 or m = 6 or m = 9 or m = 11) then
+         t := 0;
+         m:= m + 1;
+      end if;
+      -- Monatswechsel bei 31 Tagen
+      if t = 31 and (m = 1 or m = 3 or m = 5 
+                  or m = 7 or m = 8 or m = 10 or m = 12) then
+         t := 0;
+         m:= m + 1;
+      end if;
+      -- Jahreswechsel
+      if m = 13 then
+         j := j + 1;
+         m := 1;
+      end if;
+
+      t := t + 1;
+   end addiere_einen_Tag;
+   
+   tag, monat, jahr: Natural;
+   anzahl_Tage: Natural;
+begin
+   
+   Put_Line("Geben Sie das Jahr ein:");
+   Get(jahr);
+   Put_Line("Geben Sie den Monat ein:");
+   Get(monat);
+   Put_Line("Geben Sie den Tag ein:");
+   Get(tag);
+   
+   anzahl_Tage := 0;
+   while not ist_weihnachten(tag, monat) loop
+      addiere_einen_Tag(tag, monat, jahr);
+      anzahl_Tage := anzahl_Tage + 1;
+   end loop;
+   
+   Put("Es verbleiben noch ");
+   Put(anzahl_Tage);
+   Put(" Tage bis Weihnachten."); 
+   
+   
+end weihnachten;
+
+-- kate: indent-width 3; indent-mode normal; dynamic-word-wrap on; line-numbers on; space-indent on; mixed-indent off
