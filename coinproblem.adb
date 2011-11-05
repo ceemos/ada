406b180c09e11873478d8f15a6c25c91475d5895
Marcel Schneider <m.schneider.neuffen@gmail.com>
2011-11-05 22:55:34 +0100

    coinproblem, so schnell gehts...

---
+++ coinproblem.adb
@@ -0,0 +1,46 @@
+-----------------------------------------------------------------------
+--  coinproblem.adb
+--  PSE Blatt 3
+--  Version:    1
+--  Datum:      05. 11. 2011
+--  Autoren:    Marcel Schneider
+-----------------------------------------------------------------------
+
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
+
+procedure coinproblem is
+   wert : Natural := 1;
+   rest : Natural;
+   elfer : Natural;
+   siebner : Natural;
+   einer : Natural;
+   
+   optimal  : Natural;
+begin
+   Put_Line ("Wert?");
+   Get(wert);
+   
+   elfer := wert / 11;
+   rest := wert mod 11;
+   siebner := rest / 7;
+   rest := rest mod 7;
+   einer := rest;
+   
+   optimal := einer + siebner + elfer;
+   
+   for e in 0 .. elfer loop
+      rest := wert - e * 11;
+      siebner := rest / 7;
+      rest := rest mod 7;
+      einer := rest;
+      if (einer + siebner + e) < optimal then
+         optimal := einer + siebner + e;
+      end if;
+   end loop;
+   
+   Put_Line (Integer'Image(optimal));
+end coinproblem;
+
+--  kate: indent-width 3; indent-mode normal; dynamic-word-wrap on; 
+--  kate: line-numbers on; space-indent on; mixed-indent off;
