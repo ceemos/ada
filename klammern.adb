026f7cee4108848d1dea94b3e916abefe36f2db4
Marcel Schneider <m.schneider.neuffen@gmail.com>
2011-11-05 22:13:23 +0100

    Klammern noch kürzer

--- klammern.adb
+++ klammern.adb
@@ -6,26 +6,26 @@
 --  Autoren:    Marcel Schneider
 -----------------------------------------------------------------------
 
-with Ada.Text_IO, Ada.Strings.Unbounded;
-use Ada.Text_IO, Ada.Strings.Unbounded;
+with Ada.Text_IO, Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
+use Ada.Text_IO, Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
 procedure klammern is
    procedure pruefe_klammern_impl (ausdruck : in Unbounded_String; 
                                       index : in out Natural;
-                                   ergebnis : in out Boolean) is
+                                   ergebnis : in out Boolean;
+                              klammer_offen : in Boolean) is
    begin
-      Put_Line ("in der impl");
       while index <= Length (ausdruck) loop
          case Element (ausdruck, index) is
             when ')' => --  Diese Klammer ist unsere schliessende
-               index := index + 1;
-               ergebnis := True;
+               index := index + 1; --  Zeichen Verarbeitet
+               --  ok, wenn vorher eine Klammer aufging
+               ergebnis := klammer_offen; 
                return; 
             when '(' =>
                index := index + 1; --  Zeichen Verarbeitet
                --  Rekursiv weiterpruefen
-               pruefe_klammern_impl (ausdruck, index, ergebnis);
+               pruefe_klammern_impl (ausdruck, index, ergebnis, True);
                if not ergebnis then
-                  Put_Line ("impl fallthru");
                   return; --  Fehler in der inneren Klammer
                end if;
                --  index wurde aufs Ende der inneren Klammer geschoben
@@ -33,39 +33,24 @@ procedure klammern is
                index := index + 1; --  Zeichen Verarbeitet
          end case;
       end loop;
-      Put_Line ("Impl aus -> false");
-      ergebnis := False;
-      return; --  Ende erreicht, Fehler! (keine schliessende Klammer)
+      --  Ende erreicht, Fehler wenn eine Klammer offen ist
+      ergebnis := not klammer_offen;
+      return; 
    end pruefe_klammern_impl;
    
    function pruefe_klammern (ausdruck : Unbounded_String) return Boolean is
       index : Natural := 1;
       erg : Boolean := False;
    begin
-      while index <= Length (ausdruck) loop
-         case Element (ausdruck, index) is
-            when ')' => --  Diese Klammer ging nirgends auf
-               Put_Line ("schliessende im HP");
-               return False; 
-            when '(' =>
-               index := index + 1; --  Zeichen Verarbeitet
-               --  Rekursiv weiterpruefen
-               pruefe_klammern_impl (ausdruck, index, erg);
-               if not erg then
-                  return False; --  Fehler in der inneren Klammer
-               end if;
-               --  index wurde aufs Ende der inneren Klammer geschoben
-            when others =>
-               index := index + 1; --  Zeichen Verarbeitet
-         end case;
-      end loop;
-      return True; --  Ende erreicht, kein Fehler
-      
+      pruefe_klammern_impl (ausdruck, index, erg, False);
+      return erg;
    end pruefe_klammern;
    
-   testausdruck : String := "ab(cd(e()f()g))";
+   testausdruck : Unbounded_String;
 begin
-   if pruefe_klammern (To_Unbounded_String (testausdruck)) then
+   Put_Line ("Ausdruck:");
+   testausdruck := Get_Line;
+   if pruefe_klammern (testausdruck) then
       Put_Line ("alles ok");
    else 
       Put_Line ("Kaputte Klammern");
