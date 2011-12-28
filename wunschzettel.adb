db7a2176825e50bae8d2bd56e314dd79e4a80718
Marcel Schneider <m.schneider.neuffen@gmail.com>
2011-12-28 17:49:56 +0100

    so, geht...

--- wunschzettel.adb
+++ wunschzettel.adb
@@ -17,54 +17,46 @@ use Ada.Text_IO, Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
 with GNAT.Case_Util; use GNAT.Case_Util;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure Wunschzettel is
-   type Stadt_Record;
-   type Kind_Record;
-   type Wunsch_Record;
+   --  Wird nacher zu unflexibel
+--    type Stadt_Record;
+--    type Kind_Record;
+--    type Wunsch_Record;
+   type Element_Record;
    
-   type Stadt is access Stadt_Record;
-   type Kind is access Kind_Record;
-   type Wunsch is access Wunsch_Record;
+   type Element is access Element_Record;
    
-   type Stadt_Record is record
-      Next : Stadt;
+   type  Element_Record is record
+      Next : Element;
       Name : Unbounded_String;
-      Kinder : Kind;
+      Sub : Element;
    end record;
-   type Kind_Record is record
-      Next : Kind;
-      Name : Unbounded_String;
-      Wuensche : Wunsch;
-   end record;
-   type Wunsch_Record is record
-      Next : Wunsch;
-      Name : Unbounded_String;
-   end record;
+
    
-   function Read (File : String) return Stadt is
+   function Read (File : String) return Element is
       F : File_Type;
       Line : Unbounded_String;
       Typ : Character;
       
-      Current_Stadt : Stadt;
-      Current_Kind : Kind;
-      Current_Wunsch : Wunsch;
+      Current_Stadt : Element;
+      Current_Kind : Element;
+      Current_Wunsch : Element;
       
-      Anfang : Stadt;
+      Anfang : Element;
    begin
       Open (F, In_File, File);
       while not End_Of_File (F) loop
          Line := Get_Line (F);
-         Typ := Element (Line, 1);
+         Typ := Ada.Strings.Unbounded.Element (Line, 1);
          case Typ is
             when '-' =>
                if Current_Stadt = null then
                   --  Erste Zeile
-                  Current_Stadt := new Stadt_Record'(null, 
+                  Current_Stadt := new Element_Record'(null, 
                            Delete (Line, 1, 1),
                            null);
                   Anfang := Current_Stadt;
                else
-                  Current_Stadt.Next := new Stadt_Record'(null, 
+                  Current_Stadt.Next := new Element_Record'(null, 
                            Delete (Line, 1, 1),
                            null);
                   Current_Stadt := Current_Stadt.Next;
@@ -73,12 +65,12 @@ procedure Wunschzettel is
             when '+' =>     
                if Current_Kind = null then
                   --  Neue Stadt
-                  Current_Kind := new Kind_Record'(null, 
+                  Current_Kind := new Element_Record'(null, 
                            Delete (Line, 1, 1),
                            null);
-                  Current_Stadt.Kinder := Current_Kind;
+                  Current_Stadt.Sub := Current_Kind;
                else
-                  Current_Kind.Next := new Kind_Record'(null, 
+                  Current_Kind.Next := new Element_Record'(null, 
                            Delete (Line, 1, 1),
                            null);
                   Current_Kind := Current_Kind.Next;
@@ -87,12 +79,14 @@ procedure Wunschzettel is
             when '*' =>
                if Current_Wunsch = null then
                   --  1. Wunsch
-                  Current_Wunsch := new Wunsch_Record'(null, 
-                           Delete (Line, 1, 1));
-                  Current_Kind.Wuensche := Current_Wunsch;
+                  Current_Wunsch := new Element_Record'(null, 
+                           Delete (Line, 1, 1),
+                           null);
+                  Current_Kind.Sub := Current_Wunsch;
                else
-                  Current_Wunsch.Next := new Wunsch_Record'(null, 
-                           Delete (Line, 1, 1));
+                  Current_Wunsch.Next := new Element_Record'(null, 
+                           Delete (Line, 1, 1),
+                           null);
                   Current_Wunsch := Current_Wunsch.Next;
                end if;
             when others =>
@@ -112,10 +106,10 @@ procedure Wunschzettel is
       return F = T;
    end Matches;
    
-   procedure Print_List (Staedte : Stadt; 
+   procedure Print_List (Elemente : Element; 
                    Praefix : String := ""; 
                    Filter : Unbounded_String := To_Unbounded_String ("")) is
-      Current : Stadt := Staedte;
+      Current : Element := Elemente;
    begin
       while Current /= null loop
          if Matches (Current.Name, Filter) then
@@ -126,63 +120,89 @@ procedure Wunschzettel is
       end loop;
    end Print_List;
    
-   procedure Print_List (Kinder : Kind; 
-                   Praefix : String := ""; 
-                   Filter : Unbounded_String := To_Unbounded_String ("")) is
-      Current : Kind := Kinder;
-   begin
-      while Current /= null loop
-         if Matches (Current.Name, Filter) then
-            Put (Praefix);
-            Put_Line (Current.Name);
-         end if;
-         Current := Current.Next;
-      end loop;
-   end Print_List;
-   
-   procedure Print_List (Wuensche : Wunsch; 
-                   Praefix : String := ""; 
-                   Filter : Unbounded_String := To_Unbounded_String ("")) is
-      Current : Wunsch := Wuensche;
-   begin
-      while Current /= null loop
-         if Matches (Current.Name, Filter) then
-            Put (Praefix);
-            Put_Line (Current.Name);
-         end if;
-         Current := Current.Next;
-      end loop;
-   end Print_List;
-   
-    
-   
-begin
-   declare 
-      Liste : Stadt;
-      
+   function Choose (Liste : Element; Praefix : String := "") return Element is
       Eingabe : Character;
       Filter : Unbounded_String;
    begin
-      Liste := Read ("wunschliste.txt");
-      Print_List (Liste, " - ", To_Unbounded_String ("b"));
-      
       loop
-         Put ("Suche:");
+         New_Line;
+         Print_List (Liste, Praefix, Filter);
+         Put ("Suche [Esc=Abbruch]: ");
          Put (Filter);
          Get_Immediate (Eingabe);
-         Put ((Character'Pos (Eingabe)));
+         
+         --  Put ((Character'Pos (Eingabe)));
+         
+         --  Backspace
          if Character'Pos (Eingabe) = 127 then
             if Length (Filter) > 0 then
                Head (Filter, Length (Filter) - 1);
             end if;
+         --  LineFeed
          elsif Character'Pos (Eingabe) = 10 then
             exit;
+         --  Escape
+         elsif Character'Pos (Eingabe) = 27 then
+            return null;
          else 
             Append (Filter, Eingabe);
          end if;
+      end loop;
+      
+      New_Line;
+      
+      declare
+         Current : Element := Liste;
+      begin
+         while Current /= null loop
+            if Matches (Current.Name, Filter) then
+               return Current;
+            end if;
+            Current := Current.Next;
+         end loop;
+         --  Nichts gefunden
+         return null;
+      end;
+   end Choose;
+   
+   procedure Wait is
+      Eingabe : Character;
+   begin
+      Get_Immediate (Eingabe);
+   end Wait;
+begin
+   declare 
+      Liste : Element;
+      Current_Stadt : Element;
+      Current_Kind : Element;
+
+   begin
+      Liste := Read ("wunschliste.txt");
+
+      loop
+         Current_Stadt := Choose (Liste, "- ");
+         exit when Current_Stadt = null;
          
-         New_Line;
-         Print_List (Liste, " - ", Filter);
+         
+         Put ("Kinder aus ");
+         Put (Current_Stadt.Name);
+         Put_Line (":");
+         
+         loop
+            Current_Kind := Choose (Current_Stadt.Sub, "  + ");
+            exit when Current_Kind = null;
+            
+            New_Line;
+            Put ("Wuensche von ");
+            Put (Current_Kind.Name);
+            Put_Line (":");
+            
+            Print_List (Current_Kind.Sub, " * ");
+            
+            Put_Line ("Bel. Taste...");
+            Wait;
+         end loop;
+
       end loop;
    end;
 end Wunschzettel;
